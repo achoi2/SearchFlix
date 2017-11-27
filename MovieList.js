2f5e52100292b67f618582c7350b432731a1a271
achoi2 <andrewchoi1@hotmail.com>
2017-11-27 11:49:17 +0700

    Add files via upload

--- MovieList.js
+++ MovieList.js
@@ -3,13 +3,34 @@ import MovieCard from './MovieCard';
 import { Button } from 'bloomer';
 
 export default class MovieList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            search: ''
+        };
+    }
+
+    updateSearch(event) {
+        this.setState({search: event.target.value});
+    }
+    
     render() {
         const { movies, handleLoadMoreClick } = this.props;
-        
+        let filteredMovies = this.props.movies.filter(
+            (movie) => {
+                return movie.title.toLowerCase().indexOf(this.state.
+                search.toLowerCase()) !== -1;
+            }
+        );
         return (
             <div>
-              {movies.map(movie => <MovieCard key={movie.id} title={movie.title} />)}  
-              <Button isloading isColor="info" onClick={(e) => handleLoadMoreClick(e)}> Load More </Button>  
+            <input type="text" 
+                value={this.state.search}
+                onChange={this.updateSearch.bind(this)}/>
+                <ul>
+                    {filteredMovies.map(movie => <MovieCard key={movie.id} title={movie.title} />)}  
+                    <Button isloading isColor="info" onClick={(e) => handleLoadMoreClick(e)}> Load More </Button>  
+                </ul>
             </div>
         )
     }
