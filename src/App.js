075adb8be40f8dcdf05a33fb103e13aad2ff5ea2
Andrew <andrewchoi1@hotmail.com>
2018-08-17 22:20:55 -0400

    edited css and added new files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "bulma/css/bulma.css";
-import { Container } from "bloomer";
+import { Container, Button } from "bloomer";
 import MovieList from "./MovieList";
 
 class App extends Component {
@@ -10,28 +9,33 @@ class App extends Component {
     super(props);
     this.state = {
       movies: [],
-      loading: true,
-      page: 1
+      isloading: true,
+      page: 1,
     };
+    this.refreshMovies = this.refreshMovies.bind(this);
   }
   
  sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
   } 
     
+  async refreshMovies() {
+    this.movieResults = await this.fetchMovies(this.state.page);  
+    this.setState({
+      movies: this.movieResults,
+      isloading: false
+    }); 
+  }
+
   async fetchMovies(page) {
-    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=9710505071f8a3028ae8d5341ecf2f53?page=${page}`;
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=9710505071f8a3028ae8d5341ecf2f53&page=${page}`;
     const results = await fetch(url);
     const data = await results.json();
     return data.results;
   }
   
   async componentDidMount() {
-    this.movieResults = await this.fetchMovies(this.state.page);  
-    this.setState({
-      movies: this.movieResults,
-      loading: false
-    });
+    this.refreshMovies();
   }
   
   async loadMore() {
@@ -46,23 +50,25 @@ class App extends Component {
    
   render() {
     let content;
-    if (this.state.loading) {
+    if (this.state.isloading) {
       content = <h1> I am loading </h1>
     } else {
       content = <MovieList handleLoadMoreClick={(e) => this.loadMore(e)} movies={this.state.movies}/>
     }
     
-    return (
+
+    return (    
       <Container>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
+            <h1 className="App-title">SearchFlix</h1>
+         </header>
+         <Button isColor="primary" isSize="medium" onClick={this.refreshMovies}> Refresh </Button>
           <Container>{ content }</Container> 
         </div>
-      </Container>
+      </Container>  
     );
+  
   }
 }
 
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 import MovieCard from './MovieCard';
 import { Button } from 'bloomer';
+import { Input } from 'bloomer';
+import './MovieList.css';
 
 export default class MovieList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            search: ''
+        };
+    }
+
+    updateSearch(event) {
+        this.setState({search: event.target.value});
+    }
+    
     render() {
         const { movies, handleLoadMoreClick } = this.props;
-        
-        return (
-            <div>
-              {movies.map(movie => <MovieCard key={movie.id} title={movie.title} />)}  
-              <Button isColor="info" onClick={(e) => handleLoadMoreClick(e)}> Load More </Button>  
+        let filteredMovies = this.props.movies.filter(
+            (movie) => {
+                return movie.title.toLowerCase().indexOf(this.state.
+                search.toLowerCase()) !== -1;
+            }
+        );
+        return (            
+            <div>   
+                <Input type="text" isSize='medium' isColor='dark' placeholder='Movies'
+                    value={this.state.search}
+                    onChange={this.updateSearch.bind(this)}/>
+                <ul className="filtered-movies">
+                    {filteredMovies.map(movie => <MovieCard key={movie.id} title={movie.title} poster={movie.poster_path} overview={movie.overview} date={movie.release_date} vote={movie.vote_count} />)}   
+                </ul>
+                <Button isloading isColor="info" onClick={(e) => handleLoadMoreClick(e)}> Load More </Button>
             </div>
         )
     }
